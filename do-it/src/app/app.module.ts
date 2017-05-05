43d28552be66d155bd6d9ab987746b3c48ad2ec2
sundarcodes <sundar82@gmail.com>
2017-05-05 10:29:40 +0530

    Nav component implement

--- do-it/src/app/app.module.ts
+++ do-it/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Nav } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Nav
   ],
   imports: [
     BrowserModule,

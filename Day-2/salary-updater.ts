b4300ccb4c61a3bbbb9cad551fcf0ee88e361ca9
Ravinder <ryegamati@elementsolutions.com>
2017-05-08 17:13:32 +0530

    Bonus functionality

--- Day-2/salary-updater.ts
+++ Day-2/salary-updater.ts
@@ -5,6 +5,7 @@ let org: Organization = new Organization('ABC');
 org.createEmployees();
 org.printEmployeesInfo();
 let salaryUpgrader: SalaryUpgrader = new SalaryUpgrader();
-salaryUpgrader.incrementSalary(10, org.getEmployeeList());
+//salaryUpgrader.incrementSalary(10, org.getEmployeeList());
+salaryUpgrader.addBonus(5, org.getEmployeeList());
 org.printEmployeesInfo();
 

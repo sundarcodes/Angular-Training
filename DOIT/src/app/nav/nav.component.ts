c481f42a56a20f4dd8db1d562cfe24ae0321bfd9
Lasya Priya Sivalenka <lasyapriyas@virtusapolaris.com>
2017-05-09 16:20:02 +0530

    added styles

--- DOIT/src/app/nav/nav.component.ts
+++ DOIT/src/app/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core'
 @Component ({
     selector: 'app-nav',
     templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.css']
 })
 
 export class Nav {}
